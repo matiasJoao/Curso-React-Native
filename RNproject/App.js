31610d8e79c573744766ad0f3140dfb8acc882aa
João Victor Matias de Oliveira <joaovictor.a4@gmail.com>
2022-07-30 16:22:22 -0300

    StyleSheet Things

--- RNproject/App.js
+++ RNproject/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInputStyle}placeholder='What do you want to do?'/>
+        <Button title='add to list'/>
+      </View>
+      <View style={styles.listContainer}>
+        <Text>To do list</Text>
+      </View>
     </View>
+  
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      padding: '15%',
+    },
+    inputContainer: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
+    textInputStyle: {
+      borderBottomWidth: 1,
+      borderColor: 'black',
+      margin: '1.2%',
+    },
+    listContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginTop: '5%'
+    }
 });
